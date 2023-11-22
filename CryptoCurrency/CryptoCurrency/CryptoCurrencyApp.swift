4dc50e77264c4a56c1305f5bd91a4475fda226e7
mohamdan <engineerm86@gmail.com>
2023-11-22 11:29:30 +0200

    project setup

---
+++ CryptoCurrency/CryptoCurrency/CryptoCurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCurrencyApp.swift
+//  CryptoCurrency
+//
+//  Created by mohamdan on 22/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
