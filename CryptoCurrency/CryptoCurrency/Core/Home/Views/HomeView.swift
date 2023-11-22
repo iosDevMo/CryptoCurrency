b08d6cd1acd41def47537c2445b2d2a713f7a95b
mohamdan <engineerm86@gmail.com>
2023-11-22 14:21:45 +0200

    top moving coins UI

---
+++ CryptoCurrency/CryptoCurrency/Core/Home/Views/HomeView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  CryptoCurrency
+//
+//  Created by mohamdan on 22/11/2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        NavigationView {
+            ScrollView(.vertical, showsIndicators: false) {
+                // top movers view
+                
+                // all coins view
+                
+            }
+            .navigationTitle("Live Prices")
+        }
+        
+        
+       
+    }
+}
+
+#Preview {
+    HomeView()
+}
