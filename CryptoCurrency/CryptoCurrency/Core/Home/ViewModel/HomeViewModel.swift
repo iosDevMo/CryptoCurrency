43c0178497800a0647a0c21a52ce3dbe8a382eee
mohamdan <engineerm86@gmail.com>
2023-12-07 13:05:16 +0200

    modify networking with async await

--- CryptoCurrency/CryptoCurrency/Core/Home/ViewModel/HomeViewModel.swift
+++ CryptoCurrency/CryptoCurrency/Core/Home/ViewModel/HomeViewModel.swift
@@ -10,32 +10,24 @@ import SwiftUI
 class HomeViewModel: ObservableObject {
     @Published var coins = [Coin]()
     @Published var topMovingCoins = [Coin]()
+    private let urlString = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h&locale=en"
+    
     init(){
-        fetchCoinData()
+        loadData()
     }
-    func fetchCoinData(){
-        let urlString = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h&locale=en"
+    @MainActor
+    func fetchCoinData()async throws{
+        
         guard let url = URL(string: urlString)else{return}
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            if let error = error {
-                print("Debug: Error \(error.localizedDescription)")
-                return
-            }
-            if let response = response as? HTTPURLResponse {
-                print("Debug: Response \(response.statusCode)")
-            }
-            guard let data = data else {return}
-            do{
-                let coins = try JSONDecoder().decode([Coin].self, from: data)
-                DispatchQueue.main.async {
-                    self.coins = coins
-                }
-            }catch let error {
-                print("Debug: Failed to decode with error\(error)")
-                
-            }
-            
-        }.resume()
+        let (data, response) = try await URLSession.shared.data(from:url)
+        guard ( response as? HTTPURLResponse)?.statusCode == 200 else {return}
+        guard let coins = try? JSONDecoder().decode([Coin].self, from: data)else{return}
+        self.coins = coins
+    }
+    func loadData(){
+        Task(priority: .medium) {
+            try await fetchCoinData()
+        }
     }
     func configureTopMovingCoins () {
         let topMovers = coins.sorted(by: { $0.priceChangePercentage24H > $1.priceChangePercentage24H })
